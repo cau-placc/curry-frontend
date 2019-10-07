4dfd5fec8e5d378aff0981a54804ee83f66d9953
Jan-Hendrik Matthes <stu113804@mail.uni-kiel.de>
2019-10-07 19:16:21 +0200

    Remove unused imports from the TypeSubst module

--- src/Base/TypeSubst.hs
+++ src/Base/TypeSubst.hs
@@ -23,7 +23,6 @@ import           Data.List     (nub)
 import           Data.Maybe    (fromMaybe)
 import qualified Data.Set      as Set (Set, map)
 
-import           Base.Messages (internalError)
 import           Base.Subst
 import           Base.TopEnv
 import           Base.Types
