d59e9d34e04ba2cc2c2e832855fff366d593c207
Jan-Hendrik Matthes <stu113804@mail.uni-kiel.de>
2019-11-02 01:42:37 +0100

    Correct the HTML syntax highlighting of the dot symbol in type expressions

--- src/Html/SyntaxColoring.hs
+++ src/Html/SyntaxColoring.hs
@@ -374,7 +374,7 @@ idsTypeExpr (ArrowType   _ ty1 ty2) = concatMap idsTypeExpr [ty1, ty2]
 idsTypeExpr (ParenType        _ ty) = idsTypeExpr ty
 idsTypeExpr (ContextType   _ cx ty) = idsContext cx ++ idsTypeExpr ty
 idsTypeExpr (ForallType    _ vs ty) =
-  map (Identifier IdDeclare False . qualify) vs ++ idsTypeExpr ty
+  map (Identifier IdDeclare False . qualify) vs ++ Symbol "." : idsTypeExpr ty
 
 idsFieldDecl :: FieldDecl -> [Code]
 idsFieldDecl (FieldDecl _ ls ty) =
