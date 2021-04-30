231fce98e03fb39849fe428d3c7f2ca1238196c3
stu204767 <stu204767@mail.uni-kiel.de>
2021-04-30 20:22:29 +0200

    Update the class environment to support MPTCs https://git.ps.informatik.uni-kiel.de/theses/2021/2021-lkrueger-ma/-/issues/2
    The class environment needs a major overhaul in order to work with multi-parameter type classes, since super classes can now have fewer and reordered type variables in relation to a sub class. The class environment therefore stores an index list with each super class relation, which can be used to map the sub class types to the super class types. Super class application functions have been added for this type mapping.

--- src/Env/Class.hs
+++ src/Env/Class.hs
@@ -19,26 +19,46 @@
 module Env.Class
   ( ClassEnv, initClassEnv
   , ClassInfo, bindClassInfo, mergeClassInfo, lookupClassInfo
-  , superClasses, allSuperClasses, classMethods, hasDefaultImpl
+  , superClasses, classMethods, hasDefaultImpl, applySuperClass
+  , allSuperClasses, applyAllSuperClasses
   ) where
 
-import           Data.List       (nub, sort)
-import qualified Data.Map as Map (Map, empty, insertWith, lookup)
+import           Data.List                 (sort)
+import           Data.Containers.ListUtils (nubOrd)
+import qualified Data.Map as Map           (Map, empty, insertWith, lookup)
 
 import Curry.Base.Ident
 
 import Base.Messages (internalError)
 
-type ClassInfo = ([QualIdent], [(Ident, Bool)])
+-- TODO: Add and update comments, if this approach works
+
+-- ---------------------------------------------------------------------------
+-- Type Synonyms
+-- ---------------------------------------------------------------------------
+
+type ClassInfo = (Int, [SuperClassInfo], [(Ident, Bool)])
+
+-- The list represents the type variables of the superclass from left to right.
+-- The integers within this list are the indices of these variables in the
+-- subclass.
+-- examples: class C b => D a b c  -->  (D, [1])
+--           class C b a => D a b  -->  (D, [1, 0])
+-- Note, that for FlexibleContexts, this has to be (QualIdent, [Type])
+type SuperClassInfo = (QualIdent, [Int])
 
 type ClassEnv = Map.Map QualIdent ClassInfo
+
+-- ---------------------------------------------------------------------------
+-- Environment Building Functions
+-- ---------------------------------------------------------------------------
 
 initClassEnv :: ClassEnv
 initClassEnv = Map.empty
 
 bindClassInfo :: QualIdent -> ClassInfo -> ClassEnv -> ClassEnv
-bindClassInfo cls (sclss, ms) =
-  Map.insertWith mergeClassInfo cls (sort sclss, ms)
+bindClassInfo cls (arity, sclss, ms) =
+  Map.insertWith mergeClassInfo cls (arity, sort sclss, ms)
 
 -- We have to be careful when merging two class infos into one as hidden class
 -- declarations in interfaces provide no information about class methods. If
@@ -47,29 +67,64 @@ bindClassInfo cls (sclss, ms) =
 -- the class environment before with an empty list.
 
 mergeClassInfo :: ClassInfo -> ClassInfo -> ClassInfo
-mergeClassInfo (sclss1, ms1) (_, ms2) = (sclss1, if null ms1 then ms2 else ms1)
+mergeClassInfo (arity, sclss1, ms1) (_, _, ms2) =
+  (arity, sclss1, if null ms1 then ms2 else ms1)
+
+-- ---------------------------------------------------------------------------
+-- Simple Lookup Functions
+-- ---------------------------------------------------------------------------
 
 lookupClassInfo :: QualIdent -> ClassEnv -> Maybe ClassInfo
 lookupClassInfo = Map.lookup
 
-superClasses :: QualIdent -> ClassEnv -> [QualIdent]
-superClasses cls clsEnv = case lookupClassInfo cls clsEnv of
-  Just (sclss, _) -> sclss
-  _ -> internalError $ "Env.Classes.superClasses: " ++ show cls
+classArity :: QualIdent -> ClassEnv -> Int
+classArity cls clsEnv = case lookupClassInfo cls clsEnv of
+  Just (arity, _, _) -> arity
+  _ -> internalError $ "Env.Classes.classArity: " ++ show cls
 
-allSuperClasses :: QualIdent -> ClassEnv -> [QualIdent]
-allSuperClasses cls clsEnv = nub $ classes cls
-  where
-    classes cls' = cls' : concatMap classes (superClasses cls' clsEnv)
+superClasses :: QualIdent -> ClassEnv -> [SuperClassInfo]
+superClasses cls clsEnv = case lookupClassInfo cls clsEnv of
+  Just (_, sclss, _) -> sclss
+  _ -> internalError $ "Env.Classes.superClasses: " ++ show cls
 
 classMethods :: QualIdent -> ClassEnv -> [Ident]
 classMethods cls clsEnv = case lookupClassInfo cls clsEnv of
-  Just (_, ms) -> map fst ms
+  Just (_, _, ms) -> map fst ms
   _ -> internalError $ "Env.Classes.classMethods: " ++ show cls
 
 hasDefaultImpl :: QualIdent -> Ident -> ClassEnv -> Bool
 hasDefaultImpl cls f clsEnv = case lookupClassInfo cls clsEnv of
-  Just (_, ms) -> case lookup f ms of
+  Just (_, _, ms) -> case lookup f ms of
     Just dflt -> dflt
     Nothing -> internalError $ "Env.Classes.hasDefaultImpl: " ++ show f
   _ -> internalError $ "Env.Classes.hasDefaultImpl: " ++ show cls
+
+-- ---------------------------------------------------------------------------
+-- Super Class Application
+-- ---------------------------------------------------------------------------
+
+applySuperClass :: [a] -> SuperClassInfo -> (QualIdent, [a])
+applySuperClass tys (scls, varIs) = (scls, reorderTypes varIs)
+ where
+  -- reorderTypes :: [Int] -> [a]
+  reorderTypes []                   = []
+  reorderTypes (nextVarIx : remVIs) = tys `at` nextVarIx : reorderTypes remVIs
+
+  -- Like (!!), but with a different error message.
+  at :: [a] -> Int -> a
+  at (x : _ ) 0         = x
+  at (_ : xs) n | n > 0 = xs `at` (n - 1)
+  at _        _         =
+    internalError $ "Env.Classes.applySuperClass: " ++ show scls
+
+allSuperClasses :: QualIdent -> ClassEnv -> [SuperClassInfo]
+allSuperClasses cls clsEnv =
+  nubOrd $ allSuperClasses' (cls, [0 .. classArity cls clsEnv - 1])
+ where
+  allSuperClasses' sclsInfo@(scls, varIs) =
+    sclsInfo : concatMap (allSuperClasses' . applySuperClass varIs)
+                         (superClasses scls clsEnv)
+
+applyAllSuperClasses :: QualIdent -> [a] -> ClassEnv -> [(QualIdent, [a])]
+applyAllSuperClasses cls tys clsEnv =
+  map (applySuperClass tys) (allSuperClasses cls clsEnv)
