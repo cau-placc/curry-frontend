be79a01d09ec8739cc22560b463aae5cb6e6346e
Fredrik Wieczerkowski <fredrik.wieczerkowski@stu.uni-kiel.de>
2022-06-19 03:08:11 +0200

    Model implied language extensions

--- src/Curry/Syntax/Extension.hs
+++ src/Curry/Syntax/Extension.hs
@@ -15,12 +15,15 @@
 module Curry.Syntax.Extension
   ( -- * Extensions
     Extension (..), KnownExtension (..), classifyExtension, kielExtensions
+  , impliedExtensions, impliedClosure
     -- * Tools
   , Tool (..), classifyTool
   ) where
 
 import Data.Binary
 import Data.Char           (toUpper)
+import qualified Data.Set as Set
+import qualified Data.Set.Extra as Set
 import Control.Monad
 
 import Curry.Base.Ident    (Ident (..))
@@ -82,7 +85,7 @@ data KnownExtension
   | NegativeLiterals          -- ^ negative literals
   | NoImplicitPrelude         -- ^ no implicit import of the prelude
   | NoDataDeriving            -- ^ no implicit deriving of the Data class
-    deriving (Eq, Read, Show, Enum, Bounded)
+    deriving (Eq, Read, Show, Enum, Bounded, Ord)
 
 -- |Classifies a 'String' as an 'Extension'
 classifyExtension :: Ident -> Extension
@@ -94,6 +97,17 @@ classifyExtension i = case reads extName of
 -- |'Extension's available by Kiel's Curry compilers.
 kielExtensions :: [KnownExtension]
 kielExtensions = [AnonFreeVars, FunctionalPatterns]
+
+-- |Extensions implied by the given extension.
+impliedExtensions :: KnownExtension -> Set.Set KnownExtension
+impliedExtensions NoImplicitPrelude = Set.singleton NoDataDeriving
+impliedExtensions _                 = Set.empty
+
+-- |Extensions implied (possibly transitively) by the given extensions.
+impliedClosure :: Set.Set KnownExtension -> Set.Set KnownExtension
+impliedClosure exts | exts == exts' = exts
+                    | otherwise     = impliedClosure exts'
+  where exts' = Set.union exts $ Set.concatMap impliedExtensions exts
 
 -- |Different Curry tools which may accept compiler options.
 data Tool = KICS2 | PAKCS | CYMAKE | FRONTEND | UnknownTool String
