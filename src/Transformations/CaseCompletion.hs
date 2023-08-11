debc4084c206a2fc0f9b4934613a5b15f6be34e8
Kai Prott <kai.prott@hotmail.de>
2023-08-11 23:32:35 +0200

    Fix subtle bug in matching of string literals

--- src/Transformations/CaseCompletion.hs
+++ src/Transformations/CaseCompletion.hs
@@ -357,16 +357,15 @@ occursInBinding v (Binding w _) = v == w
 failedExpr :: Type -> Expression
 failedExpr ty = Function ty (qualifyWith preludeMIdent (mkIdent "failed")) 0
 
---TODO: Add note about arity of 0 because of the predefined functions in the Prelude
 eqExpr :: CS.Type -> IL.Type -> Expression -> Expression -> Expression
-eqExpr ty ty' e1 | IL.TypeConstructor _ [_] <- ty'
-  = Apply (Apply (Apply (Function eqListTy eqList 0)
-                    (Function dataCharDictType dataCharDict 0)) e1)
-  where eqList = qImplMethodId preludeMIdent qDataId [ty] $ mkIdent "==="
+eqExpr ty ty' e1 | CS.TypeApply listConTy _ <- ty
+  = let eqList = qImplMethodId preludeMIdent qDataId [CS.TypeApply listConTy (CS.TypeVariable 0)] $ mkIdent "==="
         eqListTy = TypeArrow (IL.TypeConstructor (qDictTypeId qDataId) [ty'])
                      (TypeArrow ty' (TypeArrow ty' boolType'))
         dataCharDict = qInstFunId preludeMIdent qDataId [charType]
         dataCharDictType = TypeArrow unitType' (IL.TypeConstructor (qDictTypeId qDataId) [charType'])
+    in Apply (Apply (Apply (Function eqListTy eqList 0)
+                    (Function dataCharDictType dataCharDict 0)) e1)
 eqExpr ty ty' e1 =
     Apply (Apply (Function eqTy eq 0) e1)
   where eq   = qImplMethodId preludeMIdent qDataId [ty] $ mkIdent "==="
